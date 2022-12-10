d4d80102de35a07ee34ed6a78b51259077c0c940
Rakaramadhan <103983741+Rakaramadhan@users.noreply.github.com>
2022-12-10 15:14:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 userbot = {
- owner: [ "6282334170916" ],
+ owner: [ "62882006212045" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -11,8 +11,8 @@ userbot = {
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
-   packname: "RIXLE-BOT OFFICIAL",
-   author: 'Arifi Razzaq',
+   packname: "Aisyah-MultiDevice",
+   author: 'KhaTheDarkness',
     setting: {
     admin: "only admin",
     group: "only group",
